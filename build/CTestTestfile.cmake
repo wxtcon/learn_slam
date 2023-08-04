99304450a00a96ac403414b6be2e1502767e575c
Xiaotong Wang <wxtcon@163.com>
2023-08-04 21:13:40 +0800

    remove build

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/wangxt/learn_slam
-# Build directory: /home/wangxt/learn_slam/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- main.cpp
+++ main.cpp
@@ -25,39 +25,36 @@ int main() {
 
     rotation_matrix = rotation_vector.toRotationMatrix();
 
-    Eigen::Vector3d v(1, 0, 0);
+    Eigen::Vector3d v(1, 0, 0); //要变换的位置
     Eigen::Vector3d v_rotation = rotation_vector * v;
     // cout << "(1, 0, 0) after rotation (by angle axis) = " << v_rotation.transpose() << endl;
 
     v_rotation = rotation_matrix * v;
-    // cout << "(1, 0, 0) after rotation (by matrix) = " << v_rotation.transpose() << endl;
+    cout << "(1, 0, 0) after rotation (by matrix) = " << v_rotation.transpose() << endl;
 
 
     Eigen::Vector3d euler_angle = rotation_matrix.eulerAngles(2, 1, 0);
     // cout << "yaw pitch roll:" << euler_angle << endl;
 
     Eigen::Isometry3d T = Eigen::Isometry3d::Identity();
-    T.rotate(rotation_vector);git 
-    cout << endl;
+    T.rotate(rotation_vector);
+    // cout << T.matrix() << endl;
     T.pretranslate(Eigen::Vector3d(1, 3, 4));
+    // cout << T.matrix() << endl;
 
+    Eigen::Vector3d v_transformed = T * v;
+    // cout << v_transformed << endl;
 
+    Eigen::Quaterniond q = Eigen::Quaterniond(rotation_vector);
+    // cout << q.coeffs() << endl; //q.coeffs()用于获取四元数的系数
 
+    q = Eigen::Quaterniond(rotation_matrix);
+    // cout << q.coeffs() << endl; //q.coeffs()用于获取四元数的系数
 
+    v_rotation = q * v;
+    // cout << v_rotation << endl;
 
-
-
-
-
-
-
-
-
-
-    
-
-    
-
+    cout << (q * Eigen::Quaterniond(0, 1, 0, 0) * q.inverse()).coeffs() << endl;
 
     
     printfSLAM();
