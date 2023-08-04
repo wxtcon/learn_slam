47dc26759e8e11db09079f76a0a54dd992afc1c0
Xiaotong Wang <wxtcon@163.com>
2023-08-04 20:47:01 +0800

    Init

---
+++ build/CMakeFiles/study.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/study.dir/main.cpp.o"
+  "study"
+  "study.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/study.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
